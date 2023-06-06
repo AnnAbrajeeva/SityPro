32c9ff5250485cc0200e19c5c5ab313ec3b5e616
Ann Abrajeeva <anuta040786@yandex.ru>
2023-06-06 10:15:58 +0500

    feat: add adaptive

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -21,7 +21,28 @@ const swiperPortfolio = new Swiper(".portfolio__slider", {
   navigation: {
     nextEl: ".portfolio-slider__next",
     prevEl: ".portfolio-slider__prev",
-  }
+  },
+
 });
 
-export { swiperHero, swiperPortfolio };
+const swiperPortfolioTablet = new Swiper(".portfolio__slider-tablet", {
+  slidesPerView: 1,
+  loop: true,
+  modules: [Pagination, Navigation],
+  pagination: {
+    el: ".portfolio-slider__pag-tablet",
+    type: "bullets",
+  },
+});
+
+const swiperServices = new Swiper(".services__slider", {
+  slidesPerView: 1,
+  loop: false,
+  modules: [Pagination],
+  pagination: {
+    el: '.services__pag',
+    type: 'bullets',
+  },
+});
+
+export { swiperHero, swiperPortfolio, swiperServices, swiperPortfolioTablet };
